aa632a123f6a2e786ff1affd35e027d3df3c2d8e
emeraldng <ifglitterthen@gmail.com>
2018-09-22 16:57:39 -0500

    añadiendo componente de ADD TASK

--- CRUD-EXAMPLE/src/app/app.module.ts
+++ CRUD-EXAMPLE/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTasksComponent
   ],
   imports: [
     BrowserModule
